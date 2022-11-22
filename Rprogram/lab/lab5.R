bed2f59c7a34cab28d30408ff0c2a4c6cc4db700
JH <gw05028@naver.com>
2022-11-22 17:24:37 +0900

    .

--- Rprogram/lab/lab5.R
+++ Rprogram/lab/lab5.R
@@ -27,8 +27,30 @@ exam3(6, 3)
 exam3(3, 3)
 
 
-#문제4
+#문제4 ★
 exam4 <- function(x, y, z){
-  y <- c("+", "-", "*", "%/%", "%%")
-  
+  result4 <- switch (EXPR = y,
+                    "+" = x + z,
+                    "-" = x - z,
+                    "*" = x * z,
+                    "%/%" = ifelse(x == 0, "오류1",
+                                   ifelse(z == 0, "오류2",
+                                          x %/% z)),
+                    "%%" = ifelse(x == 0, "오류1",
+                                  ifelse(z == 0, "오류2",
+                                         x %% z)),
+                    "규격의 연산자만 전달하세요."
+                      )
+  return(result4)
 }
+exam4(20, "+", 6)
+exam4(20, "^", 6)
+exam4(0, "%%", 6)
+exam4(20, "%/%", 0)
+exam4(20, "%%", 6)
+
+
+#문제5 ★
+exam5 <- function(num5, s = "#")
+  result5 <- if(num5)
+  return(NULL)
